0bc28c9d4259ca1d0dc47840064cdbd61b7367f6
bainss07 <t.bains046@mybvc.ca>
2025-04-05 16:58:35 -0600

    Added unit test for name display

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('displays name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Tegvir Singh Bains/i);
+  expect(nameElement).toBeInTheDocument();
 });
