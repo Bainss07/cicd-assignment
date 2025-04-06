1ebbf93421dad836acef7985dd40dd8389d539cd
bainss07 <t.bains046@mybvc.ca>
2025-04-05 19:10:34 -0600

    Added course name to the page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <h1>Tegvir Singh Bains</h1>
       <p>This is my React app for the CICD assignment.</p>
+      <p>Course: Enterprise Computing</p>
     </div>
   );
 }
